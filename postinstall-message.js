d00163bb618229b9edbcca17a272378c4a9dedba
Aral Roca <aral-rg@hotmail.com>
2023-02-19 22:22:24 +0100

    Add postinstall message

---
+++ postinstall-message.js
@@ -0,0 +1,18 @@
+console.log('\x1b[31m%s\x1b[0m', `----------------------------------\n`);
+
+console.log('\x1b[36m%s\x1b[0m', `Hi there!\n`);
+
+console.log('\x1b[36m%s\x1b[0m', `Thank you for using Next-translate and Next-translate-plugin. I'm happy to see that it's been helpful to so many people 🧡\n`);
+
+console.log('\x1b[36m%s\x1b[0m', `As much as I enjoy maintaining and developing new features for the library, it unfortunately doesn't pay the bills. If you believe in the project and would like to support its continued growth, please consider making a donation. Any amount, no matter how small, will be greatly appreciated and will help me to continue dedicating my time to this project.\n`);
+
+console.log('\x1b[36m%s\x1b[0m', `Also, I would be incredibly grateful if you could help me spread the word about the library and encourage any large companies you know to consider supporting the project.\n`);
+
+console.log('\x1b[36m%s\x1b[0m', `Thank you from the bottom of my heart 😊🤗\n`);
+
+console.log('\x1b[31m%s\x1b[0m', `Become a sponsor: https://github.com/sponsors/aralroca\n`);
+
+console.log('\x1b[31m%s\x1b[0m', `----------------------------------\n`);
+
+
+
