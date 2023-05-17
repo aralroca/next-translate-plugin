c79024133a3ffd54049d44bdea764c88ab9a8849
alvesvin <viniworks@protonmail.com>
2023-05-16 23:47:37 -0300

    Set todo old app dir test case

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -19,28 +19,6 @@ jest.mock(
 
 describe('nextTranslate', () => {
   describe('nextTranslate -> pagesInDir', () => {
-    test('uses app dir loader if pagesInDir points to app dir', () => {
-      fs.readdirSync.mockImplementationOnce(() => [])
-      fs.existsSync.mockImplementationOnce(() => true)
-
-      const config = nextTranslate({})
-
-      expect(config.webpack({})).toEqual(
-        expect.objectContaining({
-          module: {
-            rules: expect.arrayContaining([
-              expect.objectContaining({
-                use: expect.objectContaining({
-                  loader: 'next-translate-plugin/loader',
-                  options: expect.objectContaining({
-                    isAppDirNext13: true,
-                  }),
-                }),
-              }),
-            ]),
-          },
-        })
-      )
-    })
+    test.todo('uses app dir loader if pagesInDir points to app dir')
   })
 })
