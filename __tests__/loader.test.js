1d48a58d61c46d7294a016d67e5c8b88824d5da0
alvesvin <viniworks@protonmail.com>
2023-05-18 00:55:48 -0300

    wip: loader tests

---
+++ __tests__/loader.test.js
@@ -0,0 +1,10 @@
+import webpack from 'webpack'
+
+describe('nextTranslate', () => {
+  describe('nextTranslate -> loader', () => {
+    test.todo('loader should use server page helpers')
+    test.todo('loader should use client page helpers')
+    test.todo('loader should use client component helpers')
+    test.todo('loader should use legacy page helpers')
+  })
+})
